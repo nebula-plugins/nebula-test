d54a0bfe2954ec1e809b206b554a3962c368a19a
Justin Ryan <jryan@netflix.com>
2014-06-04 15:41:26 -0700

    Use .groovy files instead of .java files.

--- src/main/groovy/nebula/test/functional/internal/GradleHandleFactory.groovy
+++ src/main/groovy/nebula/test/functional/internal/GradleHandleFactory.groovy
@@ -1,8 +1,5 @@
 package nebula.test.functional.internal;
 
-import java.io.File;
-import java.util.List;
-
 public interface GradleHandleFactory {
 
     GradleHandle start(File dir, List<String> arguments);
