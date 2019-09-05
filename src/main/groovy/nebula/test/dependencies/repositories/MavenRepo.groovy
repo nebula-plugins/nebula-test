b486b333602b514ca1f2a24e3afa175d8e47052e
Martin Chalupa <mchalupa@netflix.com>
2019-09-05 13:21:32 -0700

    Generate maven-metadata.xml when creating repository for pom files

--- src/main/groovy/nebula/test/dependencies/repositories/MavenRepo.groovy
+++ src/main/groovy/nebula/test/dependencies/repositories/MavenRepo.groovy
@@ -15,7 +15,10 @@
  */
 package nebula.test.dependencies.repositories
 
+import groovy.xml.MarkupBuilder
 import nebula.test.dependencies.maven.Pom
+
+import java.text.SimpleDateFormat
 
 class MavenRepo {
     Set<Pom> poms = new HashSet<>()
@@ -32,10 +35,39 @@ class MavenRepo {
             root.mkdirs()
         }
         poms.each { Pom pom ->
-            def path = "${pom.artifact.group.replaceAll(/\./, '/')}/${pom.artifact.artifact}/${pom.artifact.version}"
+            def path = "${groupAndArtifactPath(pom)}/${pom.artifact.version}"
             def dir = new File(root, path)
             dir.mkdirs()
             new File(dir, pom.filename).text = pom.generate()
         }
+        generateMavenMetadata(poms)
+    }
+
+    private void generateMavenMetadata(Set<Pom> poms) {
+        Map<String, List<Pom>> groupedPoms = poms.groupBy { groupAndArtifactPath(it) }
+        groupedPoms.each { groupAndArtifactPath, List<Pom> pomGroup ->
+            List<Pom> sortedPoms = pomGroup.sort { it.artifact.version }
+            File metadataFile = new File(root, "$groupAndArtifactPath/maven-metadata.xml")
+            def writer = new FileWriter(metadataFile)
+            def xml = new MarkupBuilder(writer)
+            xml.metadata {
+                groupId(sortedPoms.first().artifact.group)
+                artifactId(sortedPoms.first().artifact.artifact)
+                versioning {
+                    latest(sortedPoms.last().artifact.version)
+                    release(sortedPoms.last().artifact.version)
+                    versions {
+                        sortedPoms.each {
+                            version(it.artifact.version)
+                        }
+                    }
+                }
+                lastUpdated(new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()))
+            }
+        }
+    }
+
+    private static String groupAndArtifactPath(Pom pom) {
+        "${pom.artifact.group.replaceAll(/\./, '/')}/${pom.artifact.artifact}"
     }
 }
--- src/test/groovy/nebula/test/dependencies/repositories/MavenRepoSpec.groovy
+++ src/test/groovy/nebula/test/dependencies/repositories/MavenRepoSpec.groovy
@@ -13,7 +13,9 @@ class MavenRepoSpec extends Specification {
             repo.root.deleteDir()
         }
         def example = new Pom('test.nebula', 'ourbom', '0.1.0', ArtifactType.POM)
+        def example2 = new Pom('test.nebula', 'ourbom', '0.2.0', ArtifactType.POM)
         repo.poms.add(example)
+        repo.poms.add(example2)
 
         when:
         repo.generate()
@@ -21,5 +23,15 @@ class MavenRepoSpec extends Specification {
         then:
         def pom = new File("${rootDir}/test/nebula/ourbom/0.1.0/ourbom-0.1.0.pom")
         pom.exists()
+
+        def metadataFile = new File("${rootDir}/test/nebula/ourbom/maven-metadata.xml")
+        metadataFile.exists()
+
+        def metadata = new XmlSlurper().parse(metadataFile)
+        metadata.groupId == 'test.nebula'
+        metadata.artifactId == 'ourbom'
+        metadata.versioning.latest == '0.2.0'
+        metadata.versioning.release == '0.2.0'
+        metadata.versioning.versions.children().toSet().collect { it.toString() }.containsAll(['0.1.0', '0.2.0'])
     }
 }
