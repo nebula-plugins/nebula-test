0bec29e0224570b3000b2083d0b8c23c5dcaca66
John Burns <johnburns@netflix.com>
2025-12-15 14:04:08 -0600

    switch to more standard actions setup
    fix GradleDependencyGenerator to run on testKit so that it doesn't break setup-gradle github action
    fix ClasspathAddingInitScriptBuilderFunctionalTest by moving to testkit base class
    disable MultiProjectIntegrationHelperSpec and ConcreteIntegrationSpec because non-testkit base classes don't work with the gradle actions init script

--- src/test/groovy/nebula/test/functional/internal/classpath/ClasspathAddingInitScriptBuilderFunctionalTest.groovy
+++ src/test/groovy/nebula/test/functional/internal/classpath/ClasspathAddingInitScriptBuilderFunctionalTest.groovy
@@ -1,9 +1,9 @@
 package nebula.test.functional.internal.classpath
 
-import nebula.test.IntegrationSpec
-import nebula.test.functional.ExecutionResult
+import nebula.test.IntegrationTestKitSpec
+import org.gradle.testkit.runner.BuildResult
 
-class ClasspathAddingInitScriptBuilderFunctionalTest extends IntegrationSpec {
+class ClasspathAddingInitScriptBuilderFunctionalTest extends IntegrationTestKitSpec {
     def "can use generated init script with huge amount of dependencies"() {
         given:
         File initScript = new File(projectDir, 'build/init.gradle')
@@ -18,9 +18,9 @@ task helloWorld {
 }
 """
         when:
-        ExecutionResult executionResult = runTasksSuccessfully('helloWorld', '--init-script', initScript.canonicalPath)
+        BuildResult result = runTasks('helloWorld', '--init-script', initScript.canonicalPath)
 
         then:
-        executionResult.standardOutput.contains('Hello World!')
+        result.output.contains('Hello World!')
     }
 }
