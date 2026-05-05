133eba554dabf70ef616b59652bcd821a11ee850
John Burns <johnburns@netflix.com>
2026-05-05 10:01:14 -0500

    first-class support for scala sources in testkit DSL

--- src/test/java/nebula/test/dsl/SourceSetBuilderTest.java
+++ src/test/java/nebula/test/dsl/SourceSetBuilderTest.java
@@ -12,11 +12,20 @@ public class SourceSetBuilderTest {
     private File tempDir;
 
     @Test
-    public void test() {
+    public void test_groovy() {
         SourceSetBuilder instance = new SourceSetBuilder(tempDir);
         instance.groovy("test.groovy", "testGroovy");
         assertThat(tempDir.toPath().resolve("groovy").resolve("test.groovy"))
                 .exists()
                 .hasContent("testGroovy");
+    }
+
+    @Test
+    public void test_scala() {
+        SourceSetBuilder instance = new SourceSetBuilder(tempDir);
+        instance.scala("Test.scala", "class ScalaClass");
+        assertThat(tempDir.toPath().resolve("scala").resolve("Test.scala"))
+                .exists()
+                .hasContent("class ScalaClass");
     }
 }
